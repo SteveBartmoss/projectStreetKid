ba931f592dfc324432b46040deaa0627a261da6a
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 13:49:17 -0600

    feat: Se agrego contenido para la pagina del proyecto raccoon

--- src/pages/raccoon.jsx
+++ src/pages/raccoon.jsx
@@ -8,6 +8,8 @@ export function Raccoon(){
                 <h1>Raccoon</h1>
             </div>
             <div>
+                <h2>v 0.1.0 pre-release</h2>
+
                 <h2>Cliente Rest ligero</h2>
 
                 <p>
@@ -16,7 +18,46 @@ export function Raccoon(){
                 </p>
 
                 <img src={raccoonImg} />
-                
+
+                <h2>Featurues</h2>
+
+                <ul>
+                    <li>Fast native HTTP requests</li>
+                    <li>Status, response time and size</li>
+                    <li>JSON viewer</li>
+                    <li>Minimal UI</li>
+                </ul>
+
+                <h2>Tech stack</h2>
+
+                <ul>
+                    <li>React</li>
+                    <li>Rust</li>
+                    <li>Tauri</li>
+                    <li>Reqwest</li>
+                </ul>
+
+                <h2>RoadMap</h2>
+
+                <ul>
+                    <li>Mejoras UI</li>
+                    <li>Sistemas de ventanas</li>
+                    <li>Configuracion de Auth</li>
+                    <li>Configuracion de parametros</li>
+                    <li>Historial de peticiones</li>
+                </ul>
+
+                <h2>Download</h2>
+
+                <p>
+                    Por ahora las plataformas soportadas para la beta son las siguientes
+                </p>
+
+                <ul>
+                    <li>Linux</li>
+                </ul>
+
+                <p>Descarga el pre-realse <a href='https://github.com/SteveBartmoss/projectRacoon/releases/tag/v0.1.0' rel="noreferrer" target="_blank" >aqui</a></p>
             </div>
         </div>
     )
