12af72c5a7dc56683bc67a0a7965bd1a965de09e
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 13:53:43 -0600

    feat: Se implemento la ruta para el proyecto raccoon

--- src/router/mainRouter.jsx
+++ src/router/mainRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
 import { MainLayout } from "../layouts/mainLayout";
 import { Projects } from "../pages/projects";
+import { Raccoon } from "../pages/raccoon";
 
 
 export function MainRouter() {
@@ -12,6 +13,7 @@ export function MainRouter() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/projects" element={<Projects />} />
+                    <Route path="/raccoon" element={<Raccoon />} />
                 </Routes>
             </MainLayout>
         </BrowserRouter>
