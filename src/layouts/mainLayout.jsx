620bd23ad737e98e2634560480dc8a274fd82f48
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 13:05:59 -0600

    feat: Se implemento el topbar para el layout de la aplicacion

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,9 +1,11 @@
+import { TopBar } from "../components/topbar/topBar";
 
 
 export function MainLayout({children}){
 
     return(
         <div>
+            <TopBar />
             {children}
         </div>
     )
