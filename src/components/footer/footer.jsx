95960273dcda1569edf0ff4ff1a63b076c1b6d36
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-23 14:48:13 -0600

    feat: Se implemento la nueva seccion para el chnagelog

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
     return (
         <div>
             <footer className="text-center text-gray-500 text-sm py-6 border-t border-gray-800">
-                © {new Date().getFullYear()} Marco. Todos los derechos reservados.
+                © {new Date().getFullYear()} Steve. Todos los derechos reservados.
             </footer>
         </div>
     )
