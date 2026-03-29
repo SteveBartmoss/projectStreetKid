b83d728064cf9c850f8c396f1b7794ee129cfea3
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-28 23:28:21 -0600

    feat: Se implemento una ruta para el proyecto neofetch

--- src/router/mainRouter.jsx
+++ src/router/mainRouter.jsx
@@ -3,6 +3,7 @@ import { Home } from "../pages/home";
 import { MainLayout } from "../layouts/mainLayout";
 import { Projects } from "../pages/projects";
 import { Raccoon } from "../pages/raccoon";
+import { NeoFetch } from "../pages/neofetch";
 
 
 export function MainRouter() {
@@ -14,6 +15,7 @@ export function MainRouter() {
                     <Route path="/" element={<Home />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/raccoon" element={<Raccoon />} />
+                    <Route path="/neofetch" element={<NeoFetch />} />
                 </Routes>
             </MainLayout>
         </BrowserRouter>
