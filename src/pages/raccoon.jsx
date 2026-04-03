84fe2b6e2b457ad8344bf07268b5dcdef93e2559
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-03 16:27:46 -0600

    feat: Se cambio el contenido para la aplicacion de raccoon

--- src/pages/raccoon.jsx
+++ src/pages/raccoon.jsx
@@ -9,7 +9,7 @@ export function Raccoon() {
             <section className="hero">
                 <div className="hero-text">
                     <h1>Raccoon</h1>
-                    <p className="version">v0.13.2 pre-release</p>
+                    <p className="version">v1.0.0 latest</p>
 
                     <h2>Cliente REST ligero y nativo</h2>
 
@@ -19,7 +19,7 @@ export function Raccoon() {
                     </p>
 
                     <a
-                        href='https://github.com/SteveBartmoss/projectRacoon/releases/tag/v0.1.0'
+                        href='https://github.com/SteveBartmoss/projectRacoon/releases/tag/v1.0.0'
                         target="_blank"
                         rel="noreferrer"
                         className="btn-primary"
@@ -41,6 +41,9 @@ export function Raccoon() {
                     <div className="card">📊 Status, response time and size</div>
                     <div className="card">🧾 JSON viewer</div>
                     <div className="card">🎯 Minimal UI</div>
+                    <div className="card">🪟 Sistema de ventanas</div>
+                    <div className="card">🔐 Auth</div>
+                    <div className="card">⚙️ Parámetros dinámicos</div>
                 </div>
             </section>
 
@@ -64,24 +67,36 @@ export function Raccoon() {
                     </div>
 
                     <div className="card">
-                        🪟 Sistema de ventanas
-                        <p className="card-sub">Multi-tabs y manejo de vistas</p>
-                    </div>
-
-                    <div className="card">
-                        🔐 Auth
-                        <p className="card-sub">Soporte para tokens y headers</p>
-                    </div>
-
-                    <div className="card">
-                        ⚙️ Parámetros dinámicos
-                        <p className="card-sub">Gestión avanzada de requests</p>
-                    </div>
-
-                    <div className="card">
                         📜 Historial
                         <p className="card-sub">Registro de peticiones</p>
                     </div>
+
+                    <div className="card">
+                        ⚙️ Descargar respuesta
+                        <p className="card-sub">Manejo de descarga de un archivo con la respuesta</p>
+                    </div>
+
+                    <div className="card">
+                        ⚙️ Manejo de Errores
+                        <p className="card-sub">Mostrar errores ocurridos</p>
+                    </div>
+
+                    <div className="card">
+                        ⚙️ Colleciones
+                        <p className="card-sub">Permitir crear colleciones de peticiones</p>
+                    </div>
+
+                    <div className="card">
+                        ⚙️ Variables de entorno
+                        <p className="card-sub">Permitir crear variables de entorno</p>
+                    </div>
+
+
+                    <div className="card">
+                        ⚙️ Guardado de peticiones
+                        <p className="card-sub">Permitir guardar peticiones</p>
+                    </div>
+
                 </div>
             </section>
 
@@ -199,8 +214,35 @@ export function Raccoon() {
                     <div className="changelog-item">
 
                         <div className="changelog-header">
-                            <span className="version-badge">v0.13.2</span>
+                            <span className="version-badge">v1.0.0</span>
                             <span className="version-date">Latest</span>
+                        </div>
+
+                        <p className="changelog-description">
+                            Mejoras visuales para el branding de la aplicacion y mejor aspecto para el usuario.
+                        </p>
+
+                        <ul className="changelog-features">
+                            <li>🎨 Icono personalizado para la aplicacion</li>
+                            <li>🎨 Se removio por completo el scroll vertial y horizontal</li>
+                        </ul>
+
+                        <a
+                            href="https://github.com/SteveBartmoss/projectRacoon/releases"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="changelog-link"
+                        >
+                            Ver release →
+                        </a>
+
+                    </div>
+
+                    <div className="changelog-item">
+
+                        <div className="changelog-header">
+                            <span className="version-badge">v0.13.2</span>
+                            <span className="version-date">pre-release</span>
                         </div>
 
                         <p className="changelog-description">
@@ -229,7 +271,7 @@ export function Raccoon() {
 
                         <div className="changelog-header">
                             <span className="version-badge">v0.9.0</span>
-                            <span className="version-date">Latest</span>
+                            <span className="version-date">pre-release</span>
                         </div>
 
                         <p className="changelog-description">
