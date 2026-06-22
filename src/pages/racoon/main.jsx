0be35bd88537a5e74b604ad0fe66afa855187dfc
steve <za19012397@zapopan.tecmm.edu.mx>
2026-06-21 19:27:51 -0600

    feat: Se agrega el archivo principal para la seccion de racoon, ya que se estaba mezclando demasiado con el otro contenido de la pagina

---
+++ src/pages/racoon/main.jsx
@@ -0,0 +1,14 @@
+import { ParticlesSnakeRacional } from '../../components/particles/particlesSnakeRacional'
+import './css/main.css'
+
+
+export function MainRacoon(){
+
+    <>
+        <ParticlesSnakeRacional>
+            <div className="racoon-container">
+                
+            </div>
+        </ParticlesSnakeRacional>
+    </>
+}
