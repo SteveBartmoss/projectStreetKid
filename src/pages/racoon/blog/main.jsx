fb2fa8d4a2574e3702c89802b96deca53ad14800
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-06-24 09:35:18 -0600

    feat: Se inicializa la estructura para la pagina de blogs principal

---
+++ src/pages/racoon/blog/main.jsx
@@ -0,0 +1,15 @@
+
+
+export function BlogRacoon() {
+
+    return(
+        <>
+        <div>
+            <div>Left aside</div>
+            <div>Center</div>
+            <div>Right aside</div>
+        </div>
+        </>
+    )
+
+}
