08ee240f4f76b917255f9a572016064b1d7063d4
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2026-08-10 11:54:17 -0600

    feat: Se agrega un archivo para el primer post

---
+++ src/pages/racoon/blog/post/updates100826.jsx
@@ -0,0 +1,13 @@
+
+
+export function Updates100826() {
+
+    return(
+        <>
+            <div>
+                <h1>Novedades en la aplicacion</h1>
+            </div>
+        </>
+    )
+
+}
